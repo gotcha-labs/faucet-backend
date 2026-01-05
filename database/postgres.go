389354b34ae1863575fd0ab37de1c28b4a2a0da4
Mario Yaksetig <myaksetig@gmail.com>
2026-01-05 18:09:33 -0500

    Update postgres.go

--- database/postgres.go
+++ database/postgres.go
@@ -1,1 +1,39 @@
+package database
 
+import (
+	"faucet-backend/models"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	log.Println("✅ Connected to PostgreSQL")
+}
+
+func Migrate() {
+	err := DB.AutoMigrate(&models.Token{}, &models.Drip{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+	log.Println("✅ Database migrated")
+}
